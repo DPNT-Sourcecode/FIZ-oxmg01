3b967e24ebd605695ef15a7e7f0f36d0861743c6
sbx_user1051 <sbx_user1051@169.254.9.69>
2022-04-04 19:49:07 +0000

    Mon Apr 04 19:49:07 UTC 2022

--- src/test/java/befaster/solutions/fizzBuzz/fizzBuzzTest.java
+++ src/test/java/befaster/solutions/fizzBuzz/fizzBuzzTest.java
@@ -1,2 +1,28 @@
-package befaster.solutions.fizzBuzz;public class fizzBuzzTest {
+package befaster.solutions.fizzBuzz;
+
+import befaster.solutions.FIZ.FizzBuzzSolution;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class fizzBuzzTest {
+    private FizzBuzzSolution sol;
+
+    @Before
+    public void setUp() {
+
+        sol = new FizzBuzzSolution();
+    }
+
+    @Test
+    public void compute_fizzbuzz() {
+        assertThat(sol.fizzBuzz(3), equalTo("fizz"));
+        assertThat(sol.fizzBuzz(5), equalTo("buzz"));
+        assertThat(sol.fizzBuzz(30), equalTo("fizz buzz"));
+        assertThat(sol.fizzBuzz(1),equalTo("1"));
+
+    }
 }
+
