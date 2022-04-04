41c64e068bce83c5f039fc9045cf52a298061e1c
sbx_user1051 <sbx_user1051@169.254.9.69>
2022-04-04 19:34:04 +0000

    Mon Apr 04 19:34:04 UTC 2022

--- src/test/java/befaster/solutions/SUM/SumSolutionTest.java
+++ src/test/java/befaster/solutions/SUM/SumSolutionTest.java
@@ -18,5 +18,7 @@ public class SumSolutionTest {
     @Test
     public void compute_sum() {
         assertThat(sum.compute(1, 1), equalTo(2));
+        assertThat(sum.compute(1, 2), equalTo(3));
     }
 }
+
